27731b02635271078b8764f2a2c33defb8241c3f
Dave86dev <davidochando1986@gmail.com>
2021-10-01 10:54:48 +0200

    Kick off control teclas, implementación correcta

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+const keyEvent = (event) => {
+
+    switch(event.keyCode){
+        case 32:
+            console.log("pulsaste la barra espaciadora");
+            
+        break;
+
+        case 40:
+            console.log("pulsaste la flecha hacia abajo");
+            
+        break;
+
+        case 38:
+            console.log("pulsaste la flecha hacia arriba");
+            
+        break;
+
+        default:
+        
+        break;
+    }
+
+   
+}
